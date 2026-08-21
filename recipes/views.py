03479130c8fead2a31945964abcaba8f6005d3c8
Eduardo <eduardo.costa1996@hotmail.com>
2026-08-20 21:54:07 -0300

    Criando a header, separando um "partial" para head e usando include no template

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request,  'global/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Eduardo'
+    })
 
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
 
 # Create your views here.
