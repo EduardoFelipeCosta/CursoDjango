90d4ef25f1b9113a4d524578c87ab719b4e797d0
Eduardo <eduardo.costa1996@hotmail.com>
2026-08-24 10:30:05 -0300

    static files

--- projeto/settings.py
+++ projeto/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+STATIC_ROOT = BASE_DIR / 'Static'
 
 
 # Email
