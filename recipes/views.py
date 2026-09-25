aeab5d08670fc40d4889e66fcd123fce3f086d08
Eduardo <eduardo.costa1996@hotmail.com>
2026-09-25 15:59:39 -0300

    Aprendendo PER_PAGE

--- recipes/views.py
+++ recipes/views.py
@@ -5,12 +5,17 @@ from django.db.models import Q
 from django.core.paginator import Paginator
 from utils.pagination import make_pagination
 
+import os
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+print(PER_PAGE, type(PER_PAGE))
+
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -33,7 +38,7 @@ def category(request, category_id):
                  f'Recipes',
     }
 
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     
     return render(request, 'recipes/pages/category.html', context={
@@ -71,7 +76,7 @@ def search(request):
         
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, 9)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
         
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}"',
